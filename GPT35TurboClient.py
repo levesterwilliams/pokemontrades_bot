522af185673cd77c9e5354749191368592eeff60
Levester Williams <levesterwilliams@gmail.com>
2024-05-04 02:15:57 -0400

    Add class method headers.

--- GPT35TurboClient.py
+++ GPT35TurboClient.py
@@ -3,6 +3,16 @@ import os
 
 class GPT35TurboClient:
     def __init__(self, secret_key, model_name):
+        """
+        Constructor to initialize GPT35TurboClient.
+
+        Args:
+            secret_key (string): API key for GPT35Turbo.
+            model_name (string): Finetuned model name for GPT35Turbo.
+
+        Raise:
+            ValueError: If secret_key or model_name is not a string
+        """
         if not isinstance(secret_key, str) or not isinstance(model_name, str):
             raise TypeError("String is expected as an argument for "
                             "GPT35TurboClient.")
@@ -12,6 +22,23 @@ class GPT35TurboClient:
         self._finetuned_model = model_name
 
     def generate_jsonstring(self, prompt):
+        """
+        Generate a JSON-formatted string that can be used as input for data
+        parsing in main application.
+
+        Args:
+            prompt (str): The prompt is input for finetuned GPT 3.5 turbo model.
+
+        Returns:
+            str: The JSON-formatted string.
+
+        Raise:
+            ValueError: If the prompt is not a string.
+
+        Note:
+            The client/caller of the method must catch and handle errors
+            associated with invoking OpenAI API requests.
+        """
         client = openai.OpenAI()
         if not isinstance(prompt, str):
             raise TypeError("String is expected as the argument for 'prompt'.")
